fd7fdf729d3b02c4d5f581af17fc36d2261f6e20
Jhonbeltran <jbeltranleon@gmail.com>
2016-11-09 23:39:03 -0500

    Plantilla de laravel, con materialize, laravel collective, carpeta de layout y de una vista

---
+++ resources/views/producto/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.principal')
+
+<?php $message=Session::get('message')?>
+
+@if($message == 'store')
+	<script>
+		alert('Agregado!');
+	</script>	
+@endif
+@if($message == 'edit')
+	<script>
+		alert('Editado!');
+	</script>	
+@endif
+@if($message == 'delete')
+	<script>
+		alert('Eliminado');
+	</script>	
+@endif
+
+@section('content')
+<div class="container">
+	<div class="row center">
+		<div class="col s12">
+			<h5 class=" card-panel">Registro de Productos</h5>
+		</div>
+	</div>
+	<table class="highlight centered">
+		<thead>
+			<th>Id</th>
+			<th>Nombre</th>
+			<th>Operación</th>
+		</thead>
+		@foreach($productos as $producto)
+		<tbody>
+			<td>{{$producto->id}}</td>
+			<td>{{$producto->nombre}}</td>
+			<td>
+				{!!link_to_route('producto.edit', $title = 'Editar', $parameters = $producto->id, $attributes = ['class'=>'btn-flat waves-effect waves-red'])!!}
+			</td>
+		</tbody>
+		@endforeach
+	</table>
+</div>
+@stop
